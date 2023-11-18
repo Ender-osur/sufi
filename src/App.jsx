f9586e617078f92269bcc09b1138f6106e0a8f66
ender-osur <ender.ga10@gmail.com>
2023-11-18 11:41:31 -0500

    Prototipo: background de la página con las imágenes y aplicación de algunos estilos a los elementos del main

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,45 @@ export default function App () {
   return (
     <>
       <div>
-        <img src='src\assets\dots_icon1.png' alt='puntos' className='images' />
-        <img src='src\assets\dots_icon1.png' alt='puntos' className='images' />
-        <img src='src\assets\elipse_icon1.png' alt='elipse' className='images' />
-        <img src='src\assets\elipse_icon1.png' alt='elipse' className='images' />
-        <img src='src\assets\triag_icon.png' alt='triag' className='images' />
-        <img src='src\assets\triag_icon.png' alt='triag' className='images' />
+        <img src='src/assets/dots_icon1.png' alt='puntos' className='images' id='punto-der' />
+        <img src='src/assets/dots_icon1.png' alt='puntos' className='images' id='punto-izq' />
+        <img src='src/assets/elipse_icon1.png' alt='elipse' className='images' id='elipse-der' />
+        <img src='src/assets/elipse_icon1.png' alt='elipse' className='images' id='elipse-izq' />
+        <img src='src/assets/triag_icon.png' alt='triag' className='images' id='triag-der' />
+        <img src='src/assets/triag_icon.png' alt='triag' className='images' id='triag-izq' />
       </div>
       <main>
         <div id='header'>
-          <a href='#'>
+          <a id='image-logo-container' href='#'>
             <img id='image-logo' src='src\assets\sufi_iconX2.png' alt='sufi' />
           </a>
-          <h2 id='title'>Sufipay</h2>
-          <h3 id='sub-title'>administrador comercial</h3>
+          <h2 id='title'>
+            Sufipay
+          </h2>
+          <h3 id='sub-title'>
+            administrador comercial
+          </h3>
         </div>
-        <form />
         <form>
-          <fieldset>
-            <label for='username'>Usuario</label>
-            <input type='email' id='username' placeholder='username' />
+          <fieldset className='input-field'>
+            <label>
+              Usuario
+              <input type='email' id='username' placeholder='username' required />
+            </label>
           </fieldset>
-          <fieldset>
-            <label for='password'>Contraseña</label>
-            <input type='password' id='password' placeholder='contraseña' />
+          <fieldset className='input-field'>
+            <label>
+              Contraseña
+              <input type='password' id='password' placeholder='contraseña' required />
+            </label>
           </fieldset>
         </form>
-        <button type='submit' className='btn'>
-          Ingresar
+        <button type='submit' id='btn'>
+          <span id='btn-span'>Ingresar</span>
         </button>
-        <a id='recovery-field'>No recuerdo mi contraseña</a>
+        <a href='#' id='recovery-field'>
+          No recuerdo mi contraseña
+        </a>
       </main>
     </>
   )
