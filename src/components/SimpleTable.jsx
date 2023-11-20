8b2c69b6cf3e5c755b5a0a0dcbd886a24f2c8b3e
ender-osur <ender.ga10@gmail.com>
2023-11-20 15:28:10 -0500

    Solución de scroll de la tabla y desbordamiento

--- src/components/SimpleTable.jsx
+++ src/components/SimpleTable.jsx
@@ -4,18 +4,15 @@ import { API_DATA } from '../utils/Constants'
 import '../styles/SimpleTable.css'
 
 const SimpleTable = () => {
-  // Hooks
   const [columns, setColumns] = useState([])
   const [data, setData] = useState([])
 
   useEffect(() => {
     const fetchTableData = async () => {
       try {
-        // Realizar el fetch directamente aquí
         const response = await fetch(API_DATA)
         const apiData = await response.json()
 
-        // Encabezados personalizados
         const customHeaders = {
           date: 'Fecha y hora',
           number: 'Número de Reembolso',
@@ -23,13 +20,12 @@ const SimpleTable = () => {
           numberid: 'Número de Documento',
           amount: 'Monto'
         }
-        // Haciendo las columnas con los encabezados personalizados
+
         const customColumns = Object.keys(customHeaders).map(key => ({
           header: customHeaders[key],
           accessorKey: key
         }))
 
-        // Actualizando estado con las columnas construidas y los datos del fetch
         setColumns(customColumns)
         setData(apiData)
       } catch (error) {
@@ -48,26 +44,28 @@ const SimpleTable = () => {
 
   return (
     <div id='container-table'>
-      <table>
-        <thead>
-          {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => (
-                <th key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row, index) => (
-            <tr key={row.id} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
-              {row.getVisibleCells().map(cell => (
-                <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className='table-wrapper'>
+        <table>
+          <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => (
+                  <th key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row, index) => (
+              <tr key={row.id} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
+                {row.getVisibleCells().map(cell => (
+                  <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
